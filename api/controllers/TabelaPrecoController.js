56905d7f2b2368d43567c545b26cca252a2f8f10
Caio Moreno <caioketo@live.com>
2017-10-11 10:58:48 -0300

    primeiro commit

--- api/controllers/TabelaPrecoController.js
+++ api/controllers/TabelaPrecoController.js
@@ -43,11 +43,11 @@ module.exports = {
 				console.log(JSON.stringify(err));
 				return res.send(JSON.stringify(err));
 			}
-			return res.view('tabelapreco/edit.ejs', {tabelaPreco: tabelasPreco[0]});
+			return res.view('tabelaPreco/edit.ejs', {tabelaPreco: tabelasPreco[0]});
 		});
 	},
 	create: function (req, res) {
-		return res.view('tabelapreco/create.ejs');
+		return res.view('tabelaPreco/create.ejs');
 	},
 	editPost: function (req, res) {
 		TabelaPreco.update({id: req.body.id}, req.body.tabelaPreco).exec(function (err, tabelaPrecoDB) {
